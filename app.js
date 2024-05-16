ce6d9110c019beaf15d2f7328ea315cac33a0229
Debarghya Halder <156457355+debargooo@users.noreply.github.com>
2024-05-15 21:51:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 let faqboxes = document.querySelectorAll(".faqbox-extra");
-let svgs = document.querySelectorAll("svg");
+let faqs = document.querySelectorAll(".faqbox");
 
-// Loop through each SVG element and add event listener
-svgs.forEach((svg, index) => {
-    svg.addEventListener("click", () => toggleFaqBox(index));
+faqs.forEach((faqs, index) => {
+    faqs.addEventListener("click", () => toggleFaqBox(index));
 });
 
 function toggleFaqBox(index) {
-    // If the clicked FAQ box is currently displayed, hide it; otherwise, display it
     if (faqboxes[index].style.display === "flex") {
         faqboxes[index].style.display = "none";
+        
     } else {
-        // First, hide all FAQ boxes
         faqboxes.forEach(box => {
             box.style.display = "none";
         });
-        // Then, display the clicked FAQ box
         faqboxes[index].style.display = "flex";
     }
 }
